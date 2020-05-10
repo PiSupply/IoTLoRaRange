5868588a066c7b2324cdf0beacea7ad603bf0e3a
Ryan Walmsley <ryan@ryanteck.uk>
2020-05-10 13:03:43 +0100

    add firmware

---
+++ IoT LoRa Raspberry Pi Node pHAT/Firmware/reset-gpio.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+if [ ! -d "/sys/class/gpio/gpio17" ]; then
+    echo "17" > /sys/class/gpio/export
+fi
+echo "out" > /sys/class/gpio/gpio17/direction
+echo "1" > /sys/class/gpio/gpio17/value
+sleep 1
+echo "0" > /sys/class/gpio/gpio17/value
+sleep 1
