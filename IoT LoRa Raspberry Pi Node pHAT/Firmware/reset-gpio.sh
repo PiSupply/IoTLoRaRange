b965d88a5b4cc08df67b826a2a0c8b9b37e6f229
Ryan Walmsley <ryan@ryanteck.uk>
2020-05-10 13:44:15 +0100

    Swap the gpio

--- IoT LoRa Raspberry Pi Node pHAT/Firmware/reset-gpio.sh
+++ IoT LoRa Raspberry Pi Node pHAT/Firmware/reset-gpio.sh
@@ -4,9 +4,9 @@ if [ ! -d "/sys/class/gpio/gpio17" ]; then
     echo "17" > /sys/class/gpio/export
 fi
 echo "out" > /sys/class/gpio/gpio17/direction
-echo "1" > /sys/class/gpio/gpio17/value
-sleep 1
 echo "0" > /sys/class/gpio/gpio17/value
+sleep 1
+echo "1" > /sys/class/gpio/gpio17/value
 sleep 1
 
 echo "Device should be in bootloader"
